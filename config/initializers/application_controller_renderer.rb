6bbfd22cd02f00b8b1613b0428ce45776a9049a8
v24kuon <v24kuong@gmail.com>
2020-05-16 02:27:42 +0900

    3s,f

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,10 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+
+Refile.secret_key = '658fac8d57a91d747cf520d55ce88d99a27bae5b9ba51d2a6b1e58f760938f81a28f3ddbe4d6b8a24576c4e6dc361d771e5926b178a889301b2d41116037323d'
